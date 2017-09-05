2b5723d99ba2acb074f5e2b7410b4f967c596920
zw_dev <1085383440@qq.com>
2017-09-06 00:41:47 +0800

    news

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,12 @@
 class Answer < ApplicationRecord
 	belongs_to :question
+
+	def is_correct
+		return self.correct == true ? '正确':'错误';
+	end
+	
+	def is_correct_css_class
+		return self.correct == true ? 'correct-answer':'wrong-answer';
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905062401) do
+ActiveRecord::Schema.define(version: 20170905081518) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20170905062401) do
     t.datetime "updated_at", null: false
     t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "exams", force: :cascade do |t|
   end
 
   create_table "papers", force: :cascade do |t|
